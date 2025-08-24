563608c84c19c45aa44d03bf4136fd64bbba34cc
tofu345 <shammahayotunde1@gmail.com>
2025-08-24 20:02:39 +0100

    wip

--- src/main.c
+++ src/main.c
@@ -7,67 +7,15 @@
 #include <errno.h>
 #include <unistd.h>
 
+#include "redis.h"
+
 int main() {
-	// Disable output buffering
 	setbuf(stdout, NULL);
 	setbuf(stderr, NULL);
 
-	int server_fd;
-    socklen_t client_addr_len;
-	struct sockaddr_in client_addr;
-
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-		printf("Socket creation failed: %s...\n", strerror(errno));
-		return 1;
-	}
-
-	// Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// ensures that we don't run into 'Address already in use' errors
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(6379),
-									 .sin_addr = { htonl(INADDR_ANY) },
-									};
-
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-		printf("Bind failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		printf("Listen failed: %s \n", strerror(errno));
-		return 1;
-	}
-
-	printf("Waiting for a client to connect...\n");
-	client_addr_len = sizeof(client_addr);
-
-	int conn_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-    if (conn_fd == -1) {
-		printf("accept connection failed: %s \n", strerror(errno));
-        return 1;
+    Server* s = server_create(6379);
+    if (s == NULL || server_listen(s) != 0) {
+        return EXIT_FAILURE;
     }
-	printf("Client connected\n");
-
-    while (1) {
-        char* msg = "+PONG\r\n";
-        size_t msglen = strlen(msg);
-        size_t n = send(conn_fd, msg, msglen, 0);
-        if (n == -1) {
-            printf("read from connection failed: %s \n", strerror(errno));
-            return 1;
-        }
-    }
-
-    close(conn_fd);
-	close(server_fd);
-
-	return 0;
+	return EXIT_SUCCESS;
 }
